fa3f543eb22b5002deb08193a31a7d44741ad1cb
owendecicco1 <owen_decicco1@baylor.edu>
2022-10-11 14:23:12 -0500

    In class Complete

---
+++ inclasswork.py
@@ -0,0 +1,11 @@
+#[expression/  iteration     condition]
+#transformation
+#List1 = [10,20,30,40,50]
+#List2 = []
+#for x in List1:
+#if x>20:
+#x* = x
+#List2.append(x)
+
+
+#List2 = [x*x for x in list 1 if x>20]
