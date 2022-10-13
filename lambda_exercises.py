fe201a373a442113a36e81c5f6a8f80789a07e55
owendecicco1 <owen_decicco1@baylor.edu>
2022-10-13 13:58:07 -0500

    second next

---
+++ lambda_exercises.py
@@ -0,0 +1,110 @@
+'''
+1)
+Write a Python program to filter a list of integers using Lambda. Go to the editor
+Original list of integers:
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Even numbers from the said list:
+[2, 4, 6, 8, 10]
+Odd numbers from the said list:
+[1, 3, 5, 7, 9]
+'''
+numbers = [1,2,3,4,5,6,7,8,9,10]
+even_numbers = list(filter(lambda x: x%2 == 0,numbers))
+print(even_numbers)
+odd_numbers = list(filter(lambda x: x%2 == 1,numbers))
+print(odd_numbers)
+
+
+
+
+''' 2)
+find which days of the week have exactly 6 characters.
+'''
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+days = list(filter(lambda num: len(num) == 6, weekdays))
+print(days)
+
+
+
+
+
+
+
+'''
+ 3)
+remove specific words from a given list 
+Original list:
+['orange', 'red', 'green', 'blue', 'white', 'black']
+
+Remove words:
+['orange', 'black']
+
+After removing the specified words from the said list:
+['red', 'green', 'blue', 'white']
+
+'''
+original_list = ["orange", "red", "green", "blue", "white", "black"]
+
+new_list = list(filter(lambda x: x!= "orange" and x!= "black",original_list))
+print(new_list)
+
+
+
+''' 4)
+ remove all elements from a given list present in another list
+Original lists:
+list1: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list2: [2, 4, 6, 8]
+
+Remove all elements from 'list1' present in 'list2:
+[1, 3, 5, 7, 9, 10]
+ '''
+ new_list = 
+
+
+
+
+
+
+
+
+''' 5)
+find the elements of a given list of strings that contain specific substring using lambda
+Original list:
+['red', 'black', 'white', 'green', 'orange']
+Substring to search:
+ack
+Elements of the said list that contain specific substring:
+['black']
+Substring to search:
+abc
+Elements of the said list that contain specific substring:
+[]
+
+'''
+
+
+
+
+
+
+''' 6)
+check whether a given string contains a capital letter, a lower case letter, a number and a minimum length of 8 characters.
+(This is like a password verification function, HINT: Python function 'any' may be useful)
+'''
+
+
+
+
+
+
+
+''' 7)
+Write a Python program to sort a list of tuples using Lambda.
+
+# Original list of tuples:
+original_scores = [('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)]
+
+# Expected Result:
+# [('Social sciences', 82), ('English', 88), ('Science', 90), ('Maths', 97)]
+'''
